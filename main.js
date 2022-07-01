bf5c7aaa34d38f2200df62d2b104e2c45d581a36
Alfons <alfonspm@gmail.com>
2022-07-01 14:10:35 +0200

    fetch API y parseacion (json)

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ function insertaDivisa(curr, selectEl){
         selectEl.appendChild(ordenacion(c.currency,c.country))
     })
     function ordenacion(value,title){
-        const o=document.createElement('opcion')
+        const o=document.createElement('option')
         o.title = title
         o.value = value
         o.text = value
-        o.className = 'nombre'
+        o.className = 'calcoption'
         return o
         }
 }
@@ -40,19 +40,6 @@ cryptoInput.onchange = ()=>calc('crypto')
 fiatselect.onchange = ()=>calc('crypto')
 crytoselect.onchange = ()=>calc()
       
-async function calc(changer){
-    const fiat = fiatselect.value
-    const cryptoVal = Number(cryptoInput.value)
-    const fiatVal = Number(fiatInput.value) 
-    const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${fiat.toLowerCase()}.json`)
-    const json = await res.json() 
-    const exchangeRate = parseInt(json.bpi[fiat].rate_float)
-    if(changer==='crypto'){
-        const amount =  cryptoVal * exchangeRate 
-        fiatInput.value = parseFloat(amount).toFixed(2)
-    }else{
-        const amount =  fiatVal / exchangeRate 
-        cryptoInput.value = amount
-        } 
-}
-      
+const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${fiat.toLowerCase()}.json`)
+const json = await res.json() 
+const exchangeRate = parseInt(json.bpi[fiat].rate_float)

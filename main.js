f682d94da7705faca2120ad94dec1b60be83b47d
Alfons <alfonspm@gmail.com>
2022-07-01 13:49:54 +0200

    CSS cutron + querys

---
+++ main.js
@@ -0,0 +1,5 @@
+const cryptoInput =document.querySelector('#crytovalue')
+    const fiatInput = document.querySelector('#fiatvalue')
+    const crytoselect = document.querySelector('#crytoselect')
+    const fiatselect = document.querySelector('#fiatselect')
+    const fiat = []
